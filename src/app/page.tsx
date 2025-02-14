ec41261d08565a75969826360bf88ea9f601d3f8
nagipragalathan <nagipragalathan@gmail.com>
2025-02-14 17:11:26 +0530

    Add Step1Form component and temporarily disable InfoPopup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 import Profile from "@/components/profile";
 import ProgressBar from "@/components/progress_bar";
 import InfoPopup from "@/components/info_popup";
-
+import Step1Form from "@/components/forms/step1_form";
 
 export default function Home() {
   return (
     <>
       <Profile />
       <ProgressBar progress={14} />
-      <InfoPopup />
+      {/* <InfoPopup /> */}
+      <Step1Form />
     </>
   );
 }
