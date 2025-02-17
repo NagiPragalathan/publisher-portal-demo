c68b7a6e753672f2600b4255b93a85724853d199
nagipragalathan <nagipragalathan@gmail.com>
2025-02-17 17:37:11 +0530

    Add dynamic image support and improve GetScore component rendering

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['ouch-cdn2.icons8.com'],
+    domains: ['static.vecteezy.com', 'ouch-cdn2.icons8.com', 'png.pngtree.com', 'cdni.iconscout.com', 'www.pngmart.com', 'www.goodfreephotos.com', 'cdn-icons-png.flaticon.com'],
   },
 };
 
--- src/components/forms/getScore.tsx
+++ src/components/forms/getScore.tsx
@@ -9,6 +9,8 @@ const GetScore = () => {
     const [currentQuestion, setCurrentQuestion] = useState("Loading...");  
     const [currentQuestionID, setCurrentQuestionID] = useState(0);
 
+    const [image, setImage] = useState("https://ouch-cdn2.icons8.com/2Q2lX6dXeKehcc62zXUkmHilb6sFftY3MkpINoBxihs/rs:fit:368:294/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMzYy/L2FjNzdmZjlmLTJj/MTMtNDNiMy1hMWY4/LTZmMjZkYTdlNWFk/ZS5zdmc.png");
+
     let questionScore: string[] = [
         "campusLifeScore",
         "internationalExposureScore",
@@ -19,6 +21,18 @@ const GetScore = () => {
         "industryConnectionScore",
         "costEffectivenessScore",
         "influenceFactor"
+    ]
+    let question_image: string[] = [
+        'https://ouch-cdn2.icons8.com/2Q2lX6dXeKehcc62zXUkmHilb6sFftY3MkpINoBxihs/rs:fit:368:294/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMzYy/L2FjNzdmZjlmLTJj/MTMtNDNiMy1hMWY4/LTZmMjZkYTdlNWFk/ZS5zdmc.png',
+        'https://png.pngtree.com/png-clipart/20230811/original/pngtree-expo-center-color-icon-vector-picture-image_7867463.png',
+        'https://cdni.iconscout.com/illustration/premium/thumb/job-placement-service-5597307-4660053.png',
+        'https://www.pngmart.com/files/21/Academic-Writing-PNG-HD-Isolated.png',
+        'https://cdni.iconscout.com/illustration/premium/thumb/mobile-infrastructure-illustration-download-in-svg-png-gif-file-formats--modern-metaverse-simulation-future-miscellaneous-pack-illustrations-5167520.png',
+        'https://png.pngtree.com/png-clipart/20230825/original/pngtree-scientific-research-laboratory-discovery-researcher-picture-image_8469717.png',
+        'https://www.goodfreephotos.com/albums/vector-images/people-connected-in-a-web-vector-clipart.png',
+        'https://cdni.iconscout.com/illustration/premium/thumb/cost-effectiveness-8770853-7141201.png',
+        'https://cdn-icons-png.flaticon.com/512/3310/3310094.png',
+        'https://static.vecteezy.com/system/resources/previews/012/011/867/non_2x/creative-character-with-dhol-free-png.png'
     ]
 
     let make_question_title = (text: string)=>{
@@ -56,29 +70,32 @@ const GetScore = () => {
         }
         const element = document.getElementById(index.toString()) as HTMLButtonElement;
         element.style.backgroundColor = colors[index];
+
+        console.log(localStorage.getItem('form_data'));
     }
     return (
         <div className="p-6 w-full flex flex-col justify-center items-center">
-        <div className="w-full max-w-[400px] flex flex-col justify-center items-center p-6 bg-[#9f9f9f87] backdrop-blur-md rounded-[20px] shadow-lg mb-6">
-            <h1 className="text-[#FFFFFF] text-[24px] font-[600] text-center mb-16">
-                {currentQuestion}
-            </h1>
-            <form className="w-full flex flex-col justify-between items-center">
-                <Image 
-                    src="https://ouch-cdn2.icons8.com/2Q2lX6dXeKehcc62zXUkmHilb6sFftY3MkpINoBxihs/rs:fit:368:294/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMzYy/L2FjNzdmZjlmLTJj/MTMtNDNiMy1hMWY4/LTZmMjZkYTdlNWFk/ZS5zdmc.png"
-                    alt="Campus Life Illustration"
-                    width={368} 
-                    height={294}
-                />
-                <div className="flex flex-row gap-2">
-                    {
-                        Array.from(colors, (_, index) => (
-                            <button key={index} id={index.toString()} className="w-10 h-10 bg-gray-400 rounded text-white font-bold" onClick={(e) => handleColorClick(e, index+1)} onMouseEnter={(e) => handleHover(e, index)}>{index+1}</button>
-                        ))
-                    }
-                </div>
-            </form>
-        </div>
+            <div className="w-full max-w-[400px] flex flex-col justify-center items-center p-6 bg-[#9f9f9f87] backdrop-blur-md rounded-[20px] shadow-lg mb-6">
+                <h1 className="text-[#FFFFFF] text-[24px] font-[600] text-center mb-16">
+                    {currentQuestion}
+                </h1>
+                <form className="w-full flex flex-col justify-between items-center">
+                    <Image 
+                        src={question_image[question]}
+                        alt="Campus Life Illustration"
+                        width={368} 
+                        height={294}
+                        className='w-[200px] h-[200px] object-contain mb-6'
+                    />
+                    <div className="flex flex-row gap-2">
+                        {
+                            Array.from(colors, (_, index) => (
+                                <button key={index} id={index.toString()} className="w-10 h-10 bg-gray-400 rounded text-white font-bold" onClick={(e) => handleColorClick(e, index+1)} onMouseEnter={(e) => handleHover(e, index)}>{index+1}</button>
+                            ))
+                        }
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
